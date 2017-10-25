a77955dbd221c77584298cd952dfad935dff6247
Anand Chitipothu <anandology@gmail.com>
2017-10-25 14:47:43 +0530

    Added description to setup.py and added setup.cfg and MANIFEST.in.

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+"""
+rorolite
+========
+
+rorolite is a command-line tool to deploy ML applications written in Python to your own server with a single command. It is an open-source tool licensed under Apache license.
+
+The interface of ``rorolite`` is based on the interface of `rorodata platform <http://rorodata.com>`_. While ``rorolite`` is limited to running programs on already created server, the [rorodata platform][rorodata] allows allocating compute resources on demand and provides more tools to streamline data science.
+
+Currently ``rorolite`` is limited to deploying one project per server.
+
+See `<https://github.com/rorodata/rorolite>`_ for more details.
+
+"""
 from setuptools import setup, find_packages
 import os.path
 
@@ -8,6 +21,8 @@ setup(
     version=__version__,
     author='rorodata',
     author_email='rorodata.team@gmail.com',
+    description='Command-line tool to deploy ML applications to your own server with a single command',
+    long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
