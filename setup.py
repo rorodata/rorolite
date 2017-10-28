9738db128817d67fe394c337d76144af6ad2ab18
Anand Chitipothu <anandology@gmail.com>
2017-10-28 13:00:24 +0530

    Bumped the version to 0.1.3.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ See `<https://github.com/rorodata/rorolite>`_ for more details.
 from setuptools import setup, find_packages
 import os.path
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 setup(
     name='rorolite',
